0ce2d2b413ad57ece61b9c444953e40c68f1bcdf
N0zzy <ffnn83@yandex.ru>
2023-06-18 21:51:11 +0500

    add sql 2

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-06-18 16:49:24 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-06-18T16:49:24+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
